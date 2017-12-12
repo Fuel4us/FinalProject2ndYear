3f591c35a28fd2a46c29f4afa2320f6dc7950c6c
Pedro <1161028@isep.ipp.pt>
2017-12-12 14:47:11 +0000

    [Implementation] Initial class definition :: Project

---
+++ src/main/java/lapr/project/model/Project.java
@@ -0,0 +1,21 @@
+package lapr.project.model;
+
+/**
+ * <p>
+ * A project is a container of road networks
+ * and allows several analysis tools to be used
+ * </p>
+ */
+public class Project {
+
+    private String name;
+    private String description;
+    private RoadNetwork roadNetwork;
+
+    public Project(String name, String description, RoadNetwork roadNetwork) {
+        this.name = name;
+        this.description = description;
+        this.roadNetwork = roadNetwork;
+    }
+
+}
