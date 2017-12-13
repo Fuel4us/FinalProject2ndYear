b3905e8ba90839c836794938ea29814002afae2b
--replace-all <1161564@isep.ipp.pt>
2017-12-13 11:07:06 +0000

    [Implementation][UIs] Created initializer to help reduce code duplication generated by NetBeans swing

---
+++ src/main/java/lapr/project/ui/InitializeUIElements.java
@@ -0,0 +1,33 @@
+package lapr.project.ui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+/**
+ * Created by Ana Rita on 12/12/2017
+ */
+public class InitializeUIElements {
+
+    void initializeLabels(JLabel label, Font font, String text, int hAlignment, Color color){
+        label.setFont(font);
+        label.setText(text);
+        label.setForeground(color);
+        label.setHorizontalAlignment(hAlignment);
+    }
+
+    void initializeJButton(JButton button, Font font, String text, Color color, BorderFactory border){
+        button.setBackground(color);
+        button.setFont(font);
+        button.setText(text);
+        button.setBorder((Border) border);
+    }
+
+    void initializeJList(JList list, ListSelectionModel selectionModel, Color background, Color foreground, BorderFactory border, Font font, String text){
+        list.setBackground(background);
+        list.setForeground(foreground);
+        list.setBorder(border);
+        list.setFont(font);
+        list.setSelectionModel(selectionModel);
+    }
+}
