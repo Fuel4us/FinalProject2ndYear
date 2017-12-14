025ed006dc1757aa0407e17a3b5771d5595f1bce
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-14 17:00:39 +0000

    [UnitTest] Added the test ensureCreationProject to the method createProject of the class CreateProjectController

--- src/test/java/lapr/project/controller/CreateProjectControllerTest.java
+++ src/test/java/lapr/project/controller/CreateProjectControllerTest.java
@@ -1,5 +1,14 @@
 package lapr.project.controller;
 
+import lapr.project.model.*;
+import lapr.project.model.RoadNetwork.*;
+import lapr.project.model.Vehicle.*;
+import lapr.project.utils.Graph.Graph;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,5 +16,27 @@ import static org.junit.Assert.*;
  * that represents the Controller for the UC2 Create Project
  */
 public class CreateProjectControllerTest {
+
+    /**
+     * Ensures the new object Project is created successfully
+     * Method createProject()
+     * @throws Exception
+     */
+    @Test
+    public void ensureCreationProject() throws Exception {
+
+        CreateProjectController controller = new CreateProjectController(new DataBaseCommunicator());
+
+        String nameTest = "Project 1";
+        String descriptionTest = "Description 1";
+        RoadNetwork<Road, Section> roadNetworkTest = new RoadNetwork<>(false, new Graph<>(false));
+        List<Vehicle> vehicleListTest = new ArrayList<>();
+
+        Project projectExpected = new Project(nameTest, descriptionTest, roadNetworkTest, vehicleListTest);
+
+        Project projectResult = controller.createProject(nameTest, descriptionTest);
+
+        assertEquals(projectExpected, projectResult);
+    }
 
 }
