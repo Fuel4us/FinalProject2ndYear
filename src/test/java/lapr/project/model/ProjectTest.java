8d06afba0e5292e06545bb97ecdc8d6dd4995dc4
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-14 17:23:17 +0000

    [UnitTest] Added the test ensureEqualsReturnsFalse that ensures the method equals from the class Project returns false

--- src/test/java/lapr/project/model/ProjectTest.java
+++ src/test/java/lapr/project/model/ProjectTest.java
@@ -37,4 +37,35 @@ public class ProjectTest {
         assertEquals(true, result);
     }
 
+    /**
+     * Ensures equals() returns false:
+     * - in the second condition
+     * - in the final return
+     * @throws Exception
+     */
+    @Test
+    public void ensureEqualsReturnsFalse() throws Exception {
+
+        //object p2 is null
+        Project p1 = new Project("Name 1", "Description 1",
+                new RoadNetwork<>(false, new Graph<>(false)), new ArrayList<>());
+
+        Project p2 = null;
+
+        boolean result = Objects.equals(p1, p2);
+
+        assertEquals(false, result);
+
+        //object p3 has the name null but the p4 doesn't
+        Project p3 = new Project(null, "Description 3",
+                new RoadNetwork<>(false, new Graph<>(false)), new ArrayList<>());
+
+        Project p4 = new Project("Name 4", "Description 4",
+                new RoadNetwork<>(false, new Graph<>(false)), new ArrayList<>());
+
+        result = Objects.equals(p3, p4);
+
+        assertEquals(false, result);
+    }
+
 }
