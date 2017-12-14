4c214433a71d20aa09109c1e88f02af280164940
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-14 15:50:47 +0000

    [UnitTest] Added the test class for the class CreateProjectController

---
+++ src/test/java/lapr/project/controller/CreateProjectControllerTest.java
@@ -0,0 +1,11 @@
+package lapr.project.controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for the class CreateProjectController,
+ * that represents the Controller for the UC2 Create Project
+ */
+public class CreateProjectControllerTest {
+
+}
