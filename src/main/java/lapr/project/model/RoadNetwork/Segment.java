4e743ffc306f1227a09126bd054de275b60b7545
--replace-all <1161564@isep.ipp.pt>
2017-12-14 10:40:32 +0000

    [LP31718G38-35][Implementation] Modifications and addition of XMLElement in class Segment

--- src/main/java/lapr/project/model/RoadNetwork/Segment.java
+++ src/main/java/lapr/project/model/RoadNetwork/Segment.java
@@ -1,24 +1,28 @@
 package lapr.project.model.RoadNetwork;
 
-/**
- * ToDo add JavaDoc
- */
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Segment {
 
+    @XmlElement(name = "id")
     private int index;
-
+    @XmlElement(name = "init_height")
     private double initialHeight;
+    @XmlElement(name = "final_height")
+    private double finalHeight;
 
     private double slope;
-
-    private double lengh;
-
+    @XmlElement
+    private double length;
+    @XmlElement(name = "wind_direction")
     private double windAngle;
-
+    @XmlElement(name = "wind_speed")
     private double windSpeed;
-
+    @XmlElement(name = "max_velocity")
     private double maxVelocity;
-
+    @XmlElement(name = "min_velocity")
     private double minVelocity;
 
     private double toll;
