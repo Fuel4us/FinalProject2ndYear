f9778a45f66731a5faf6af6dab856aaf17267f1b
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-14 13:50:22 +0000

    [Implementation] Added the attribute vehicles to the class Project

--- src/main/java/lapr/project/model/Project.java
+++ src/main/java/lapr/project/model/Project.java
@@ -1,6 +1,9 @@
 package lapr.project.model;
 
 import lapr.project.model.RoadNetwork.RoadNetwork;
+import lapr.project.model.Vehicle.AbstractVehicle;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,11 +16,13 @@ public class Project {
     private String name;
     private String description;
     private RoadNetwork roadNetwork;
+    private List<AbstractVehicle> vehicles;
 
-    public Project(String name, String description, RoadNetwork roadNetwork) {
+    public Project(String name, String description, RoadNetwork roadNetwork, List<AbstractVehicle> vehicles) {
         this.name = name;
         this.description = description;
         this.roadNetwork = roadNetwork;
+        this.vehicles = vehicles;
     }
 
 }
