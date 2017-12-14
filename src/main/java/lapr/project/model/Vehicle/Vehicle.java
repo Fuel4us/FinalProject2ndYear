5c2c9e24b158b2c19bb8e440ca11576defeea802
Gonçalo Fonseca <1150503@isep.ipp.pt>
2017-12-14 15:54:55 +0000

    [Implementation] AbstractNonCombustionVehicle [Implementation] Fix on vehicle

--- src/main/java/lapr/project/model/Vehicle/Vehicle.java
+++ src/main/java/lapr/project/model/Vehicle/Vehicle.java
@@ -1,6 +1,7 @@
 package lapr.project.model.Vehicle;
 
 import java.util.Optional;
+import javax.xml.bind.annotation.XmlElement;
 
 /**
  * <p>
@@ -10,31 +11,47 @@ import java.util.Optional;
  */
 public abstract class Vehicle {
 
+    @XmlElement
     private String name;
     
+    @XmlElement
     private String description;
     
-    private Motorization motorization;
-    
-    private Fuel fuel;
-
+    @XmlElement
     private VehicleType type;
-
+    
+    @XmlElement(name = "toll_class")
     private TollClass vehicleClass;
-
-    private double mass;
-
+    
+    @XmlElement
+    private Motorization motorization;
+   
+    @XmlElement
+    private Fuel fuel;
+    
+    @XmlElement
+    private int mass;
+    
     /**
      * Not all vehicles transport load.
      * {@link Optional} is used to force unwrap,
      * reminding the programmer that load may be null
      */
+    @XmlElement
     private Optional<Load> load;
 
+    @XmlElement(name = "drag")
     private double dragCoefficient;
-
+    
+    @XmlElement(name = "frontal_area")
+    private double frontalRear;
+    
+    @XmlElement(name = "rrc")
     private double rollingReleaseCoefficient;
 
+    @XmlElement(name = "wheel_size")
     private double wheelSize;
-
+    
+    
+    
 }
