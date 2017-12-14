b46a96811f46457fb0d65fc4ce3f639c68569068
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-14 17:01:27 +0000

    [Implementation] Added the methods equals and hashCode in the class Project

--- src/main/java/lapr/project/model/Project.java
+++ src/main/java/lapr/project/model/Project.java
@@ -65,4 +65,19 @@ public class Project {
     public List<Vehicle> getVehicles() {
         return vehicles;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Project project = (Project) o;
+
+        return name != null ? name.equals(project.name) : project.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
