3c50b82b0f77a1064177863bfd112748cc93b463
--replace-all <1161564@isep.ipp.pt>
2017-12-14 10:38:52 +0000

    [LP31718G38-35][Implementation]Modifications and addition of XMLElement in class Section

--- src/main/java/lapr/project/model/RoadNetwork/Section.java
+++ src/main/java/lapr/project/model/RoadNetwork/Section.java
@@ -1,20 +1,27 @@
 package lapr.project.model.RoadNetwork;
 
+import javax.xml.bind.annotation.*;
 import java.util.Collection;
 
-/**
- * ToDo add JavaDoc
- */
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Section {
 
+    @XmlElement(name = "begin")
     private Node beginningNode;
-
+    @XmlElement(name = "end")
     private Node endingNode;
-
+    @XmlElement
     private Direction direction;
+    @XmlElementWrapper(name = "segment_list")
+    @XmlElement(name = "segment")
+    private Collection<Segment> segments;
 
-    private Typology typology;
-
-    private Collection segments;
-
+    public Section(Node beginningNode, Node endingNode, Direction direction, Collection segments) {
+        this.beginningNode = beginningNode;
+        this.endingNode = endingNode;
+        this.direction = direction;
+        this.segments = segments;
+    }
 }
