6b8a56c0bdc126b42927ed434bfe85b128b77997
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-14 14:03:48 +0000

    [Implementation] Added the method createProject in the class CreateProjectController

--- src/main/java/lapr/project/controller/CreateProjectController.java
+++ src/main/java/lapr/project/controller/CreateProjectController.java
@@ -1,6 +1,10 @@
 package lapr.project.controller;
 
-import lapr.project.model.DataBaseCommunicator;
+import lapr.project.model.*;
+import lapr.project.model.RoadNetwork.*;
+import lapr.project.model.Vehicle.*;
+
+import java.util.List;
 
 /**
  * Class that represents the Controller for the UC2 - Create Project
@@ -8,11 +12,27 @@ import lapr.project.model.DataBaseCommunicator;
 public class CreateProjectController {
 
     private DataBaseCommunicator dbCom;
+    private Project project;
+
+    private List<AbstractVehicle> vehicles;
+    private RoadNetwork roadNetwork;
 
     /**
      * Full constructor for the class CreateProjectController
      */
     public CreateProjectController(DataBaseCommunicator dbCom) {
         this.dbCom = dbCom;
+    }
+
+//    public void importVehicles() {
+//
+//    }
+
+//    public void importRoadNetwork() {
+//
+//    }
+
+    public void createProject(String name, String description) {
+        project = new Project(name, description, roadNetwork, vehicles);
     }
 }
