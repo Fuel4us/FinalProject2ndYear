c98d64981d4a14796bfbc61b2313517c578fe69b
--replace-all <1161564@isep.ipp.pt>
2017-12-15 20:05:49 +0000

    [LP31718G38-49][Implementation] Changes in constructor and raw types :: Section

--- src/main/java/lapr/project/model/RoadNetwork/Section.java
+++ src/main/java/lapr/project/model/RoadNetwork/Section.java
@@ -1,27 +1,52 @@
 package lapr.project.model.RoadNetwork;
 
+import lapr.project.utils.Graph.Edge;
+
 import javax.xml.bind.annotation.*;
 import java.util.Collection;
 
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Section {
+public class Section extends Edge<Node, Direction> {
 
     @XmlElement(name = "begin")
-    private Node beginningNode;
+    private lapr.project.model.RoadNetwork.Node beginningNode;
     @XmlElement(name = "end")
-    private Node endingNode;
+    private lapr.project.model.RoadNetwork.Node endingNode;
     @XmlElement
     private Direction direction;
     @XmlElementWrapper(name = "segment_list")
     @XmlElement(name = "segment")
     private Collection<Segment> segments;
 
-    public Section(Node beginningNode, Node endingNode, Direction direction, Collection<Segment> segments) {
-        this.beginningNode = beginningNode;
-        this.endingNode = endingNode;
-        this.direction = direction;
+    /**
+     * Constructor
+     * @param beginningNode
+     * @param endingNode
+     * @param direction
+     * @param segments
+     */
+    public Section(lapr.project.model.RoadNetwork.Node beginningNode, lapr.project.model.RoadNetwork.Node endingNode, Direction direction, Collection<Segment> segments) {
+
+        super(direction, calculateTotalLength(segments), beginningNode, endingNode);
         this.segments = segments;
+        this.beginningNode=beginningNode;
+        this.endingNode=endingNode;
+        this.direction=direction;
     }
+
+    /**
+     * Determines the weight of the edge, equating weight with the sum of the length of each segment
+     * @param segments
+     * @return
+     */
+    private static double calculateTotalLength(Collection<Segment> segments){
+        double weight = 0.0;
+        for (Segment segment: segments) {
+            weight += segment.getLength();
+        }
+        return weight;
+    }
+
 }
