cd2467249ac6f140de40b7fac6bb0cbb6d2b448a
Pedro <1161028@isep.ipp.pt>
2017-12-16 14:47:44 +0000

    [Implementation] Added commit and rollback methods to interface declaration :: DBAccessor

--- src/main/java/lapr/project/utils/DataAccessLayer/Oracle/DBAccessor.java
+++ src/main/java/lapr/project/utils/DataAccessLayer/Oracle/DBAccessor.java
@@ -1,11 +1,14 @@
 package lapr.project.utils.DataAccessLayer.Oracle;
 
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 /**
  * Establishes basic access behaviour to a DataBase
  */
 public interface DBAccessor {
+
+    Logger DB_ACCESS_LOG = Logger.getLogger("DataBaseAccessLog");
 
     /**
      * Connects to a database
@@ -18,5 +21,16 @@ public interface DBAccessor {
      * @return true if connection is active
      */
     boolean hasActiveConnection();
-    
+
+    /**
+     * Finishes a transaction by committing changes
+     */
+    void commit() throws SQLException;
+
+    /**
+     * Rolls a transaction back
+     */
+    void rollback() throws SQLException;
+
+
 }
