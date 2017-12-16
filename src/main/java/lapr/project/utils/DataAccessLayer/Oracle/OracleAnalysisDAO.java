c2088fd3a7a48e702f894479d4e72540493be1da
Pedro <1161028@isep.ipp.pt>
2017-12-16 15:34:00 +0000

    [Implementation] Modified constructor so as to accept an instance of OracleDataSource :: OracleAnalysisDAO

--- src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleAnalysisDAO.java
+++ src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleAnalysisDAO.java
@@ -2,25 +2,30 @@ package lapr.project.utils.DataAccessLayer.Oracle;
 
 import lapr.project.model.Analysis;
 import lapr.project.utils.DataAccessLayer.Abstraction.AnalysisDAO;
+import lapr.project.utils.DataAccessLayer.Abstraction.DBAccessor;
+import oracle.jdbc.pool.OracleDataSource;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
  * Handles Data Access via OracleDB
  */
-public class OracleAnalysisDAO extends OracleDBAccessor implements AnalysisDAO {
+public class OracleAnalysisDAO implements AnalysisDAO {
 
     private PreparedStatement saveStatement;
 
-    public OracleAnalysisDAO() {
-        super();
+    public OracleAnalysisDAO(OracleDataSource oracleDataSource) {
         try {
+            Connection oracleConnection = oracleDataSource.getConnection();
             saveStatement = oracleConnection.prepareStatement(
                     "INSERT INTO ANALYSIS(ID, PROJECTNAME) VALUES (?, ?)"
             );
+            oracleConnection.commit();
+            oracleConnection.close();
         } catch (SQLException e) {
-            super.logSQLException(e);
+            DBAccessor.logSQLException(e);
         }
     }
 
@@ -29,20 +34,13 @@ public class OracleAnalysisDAO extends OracleDBAccessor implements AnalysisDAO {
      * @param analysis an instance of {@link Analysis}
      */
     @Override
-    public void storeAnalysis(Analysis analysis) {
+    public void storeAnalysis(Analysis analysis) throws SQLException {
+        int analysisID = analysis.identify();
+        saveStatement.setInt(1, analysisID);
+        saveStatement.setString(2, analysis.issueRequestingEntity().getName());
 
-        try {
-            int analysisID = analysis.identify();
-            saveStatement.setInt(1, analysisID);
-            saveStatement.setString(2, analysis.issueRequestingEntity().getName());
-
-            saveStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            super.logSQLException(e);
-        }
-
-
+        saveStatement.executeUpdate();
     }
+
 
 }
