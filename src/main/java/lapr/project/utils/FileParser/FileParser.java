210ae808fb2cc0a6b12b50c7f2fd4207f000f28a
Ana Rita <1161564@isep.ipp.pt>
2017-12-16 13:14:24 +0000

    [LP31718G38-35][LP31718G38-58][Implementation] Improvement in classes used to import xml files

---
+++ src/main/java/lapr/project/utils/FileParser/FileParser.java
@@ -0,0 +1,9 @@
+
+package lapr.project.utils.FileParser;
+
+/**
+ * Allows a careful design of the methods used for data input/output, namely using the strategy design pattern
+ */
+public interface FileParser {
+    
+}
