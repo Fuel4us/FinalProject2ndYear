0d2e7699179d4d5d4d209ff6281c018f658f45fd
Pedro <1161028@isep.ipp.pt>
2017-12-17 12:41:35 +0000

    Fixed inconsistent value declaration for color constant :: Main

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -8,39 +8,38 @@ import java.util.logging.Logger;
  */
 class Main {
 
-	static final String SEGOE_FONT = "Segoe UI Semibold";
-        static final Font FORTHYEIGHT_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 48);
-	static final Font EIGHTEEN_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 18);
-	static final Font FOURTEEN_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 14);
-	static final Font TWELVE_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 12);
-	static final Font TV_POSTER_FONT = new java.awt.Font("SF Movie Poster", 0, 48);
-	static final Color LIGHTEST_COLOR = new java.awt.Color(204,204,204);
-	static final Color DARK_GREY = new java.awt.Color(45,46,45);
-	static final Color DARK_BLUE = new java.awt.Color(97,122,133);
-	static final Color DARK_ORANGE = new java.awt.Color(250,152,60);
-	static final Color LIGHT_GRAY= new java.awt.Color(87,89,87);
-	static final Color LIGHT_BLUE= new java.awt.Color(155,177,189);
-	static final Color LIGHT_ORANGE= new java.awt.Color(2555,219,168);
+    static final String SEGOE_FONT = "Segoe UI Semibold";
+    static final Font FORTHYEIGHT_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 48);
+    static final Font EIGHTEEN_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 18);
+    static final Font FOURTEEN_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 14);
+    static final Font TWELVE_SEGOE_FONT = new java.awt.Font(SEGOE_FONT, 0, 12);
+    static final Font TV_POSTER_FONT = new java.awt.Font("SF Movie Poster", 0, 48);
+    static final Color LIGHTEST_COLOR = new java.awt.Color(204, 204, 204);
+    static final Color DARK_GREY = new java.awt.Color(45, 46, 45);
+    static final Color DARK_BLUE = new java.awt.Color(97, 122, 133);
+    static final Color DARK_ORANGE = new java.awt.Color(250, 152, 60);
+    static final Color LIGHT_GRAY = new java.awt.Color(87, 89, 87);
+    static final Color LIGHT_BLUE = new java.awt.Color(155, 177, 189);
+    static final Color LIGHT_ORANGE = new java.awt.Color(255, 219, 168);
 
-	/**
-	 * Logger class.
-	 */
-	private static final Logger LOGGER = Logger.getLogger("MainLog");
+    /**
+     * Logger class.
+     */
+    private static final Logger LOGGER = Logger.getLogger("MainLog");
 
-	/**
-	 * Private constructor to hide implicit public one.
-	 */
-	private Main() {
+    /**
+     * Private constructor to hide implicit public one.
+     */
+    private Main() {
 
-	}
+    }
 
-	/**
-	 * Application main method.
-	 * 
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		WelcomeUI.main(null);
-	}
+    /**
+     * Application main method.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WelcomeUI.main(null);
+    }
 
 }
