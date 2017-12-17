cbe938b52b8b030e7c7cf647b428227eb34fc804
pedrof111 <1160950@isep.ipp.pt>
2017-12-17 16:57:59 +0000

    [implementation] Adding segments to the createRoadNetwork implemented
    The part of the method that adds segments to the iterator segments created

--- src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleRoadNetworkDAO.java
+++ src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleRoadNetworkDAO.java
@@ -8,9 +8,14 @@ package lapr.project.utils.DataAccessLayer.Oracle;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import lapr.project.model.RoadNetwork.Direction;
 import lapr.project.model.RoadNetwork.RoadNetwork;
 import lapr.project.model.RoadNetwork.Node;
+import lapr.project.model.RoadNetwork.Road;
 import lapr.project.model.RoadNetwork.Section;
+import lapr.project.model.RoadNetwork.Segment;
 
 
 /**
@@ -31,29 +36,46 @@ public class OracleRoadNetworkDAO extends OracleDBAccessor{
     }
 
 
-    
+
     private RoadNetwork<Node, Section> createRoadNetwork(ResultSet resultSet) throws SQLException {
         //parametros das RoadNetwork
         RoadNetwork<Node, Section> roadNetwork = new RoadNetwork<>(true);
         while(resultSet.next()){
+            //select para ir buscar os nodes
             String nodeName;
             Node node;
             nodeName = resultSet.getString("id");
             node = new Node(nodeName);
             roadNetwork.addNode(node);
-        }
-        
-        /**
-        section precisa dos dois nodes origem e destino
+
+        //select pra ir buscar as sections
+        //section precisa dos dois nodes origem e destino
         Node begginningNode;
         Node endingNode;
+//ir buscar nodes
         Direction direction;
-        Collection<Segment> segments;
+        Collection<Segment> segments = new ArrayList<>();
         Road road;
-        */      
-        
+//ir buscar direction da mesma forma que eu faço com os enums do veiculo
+//select que dá um result set dos segments pertencentes a esta section
+//while que itera sobre o result set dos segments, e cria um segment, adicionando-o à section
+//select que determina qual a road desta section
+//adicionar section
+        //resultSet dos segments
+        while(resultSet.next()){
+            int index = resultSet.getInt("index");
+            double initialHeight = resultSet.getDouble("initialHeight");
+            double finalHeight = resultSet.getDouble("finalheight");
+            double length = resultSet.getDouble("length");
+            double windAngle = resultSet.getDouble("windAngle");
+            double windSpeed = resultSet.getDouble("windSpeed");
+            double maxVelocity = resultSet.getDouble("maxVelocity");
+            double minVelocity = resultSet.getDouble("minVelocity");
+            segments.add(new Segment(index,initialHeight,finalHeight,length,windAngle,windSpeed,maxVelocity,minVelocity));
+        }
+        }
         return roadNetwork;
     }
-    
+
 }
 
