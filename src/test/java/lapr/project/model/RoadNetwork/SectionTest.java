4c989fd8171873abfe8c96448ff2928b7f169dd4
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-18 19:56:59 +0000

    [UnitTest] Created the test class for the class Section

---
+++ src/test/java/lapr/project/model/RoadNetwork/SectionTest.java
@@ -0,0 +1,15 @@
+package lapr.project.model.RoadNetwork;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Represents the test class for the Section class
+ */
+public class SectionTest {
+    @Test
+    public void calculateTotalMinimumTimeInterval() throws Exception {
+    }
+
+}
