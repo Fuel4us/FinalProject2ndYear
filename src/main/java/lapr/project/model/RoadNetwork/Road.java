e3b8c207b88ed68f3bf4da5d5a06ea4170681b29
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-18 12:24:00 +0000

    [Implementation] Added the methods equals and hashCode in the class Road

--- src/main/java/lapr/project/model/RoadNetwork/Road.java
+++ src/main/java/lapr/project/model/RoadNetwork/Road.java
@@ -54,4 +54,25 @@ public class Road {
         return id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Road road = (Road) o;
+
+        if (id != null ? !id.equals(road.id) : road.id != null) return false;
+        if (name != null ? !name.equals(road.name) : road.name != null) return false;
+        if (typology != null ? !typology.equals(road.typology) : road.typology != null) return false;
+        return tollFare != null ? tollFare.equals(road.tollFare) : road.tollFare == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (typology != null ? typology.hashCode() : 0);
+        result = 31 * result + (tollFare != null ? tollFare.hashCode() : 0);
+        return result;
+    }
 }
