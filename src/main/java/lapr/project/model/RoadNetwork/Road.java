513f8ce0d8855c63e2d2c4147dd5856053d27a53
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-18 09:11:19 +0000

    [Implementation] Added the full constructor for the class Road

--- src/main/java/lapr/project/model/RoadNetwork/Road.java
+++ src/main/java/lapr/project/model/RoadNetwork/Road.java
@@ -20,6 +20,20 @@ public class Road {
     private List<Float> tollFare;
 
     /**
+     * Full constructor for the class Road
+     * @param id the road's id
+     * @param name the road's name
+     * @param typology the road's typology
+     * @param tollFare the road's toll fare
+     */
+    public Road(String id, String name, String typology, List<Float> tollFare) {
+        this.id = id;
+        this.name = name;
+        this.typology = typology;
+        this.tollFare = tollFare;
+    }
+
+    /**
      * @return the ID of this road
      */
     public String getID() {
