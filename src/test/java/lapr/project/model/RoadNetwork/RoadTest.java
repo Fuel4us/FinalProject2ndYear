06e5297abcaea5cc5a3b5e939d53580bee7efbff
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-18 12:27:02 +0000

    [UnitTest] Created the class RoadTest to test the class Road

---
+++ src/test/java/lapr/project/model/RoadNetwork/RoadTest.java
@@ -0,0 +1,35 @@
+package lapr.project.model.RoadNetwork;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RoadTest {
+
+    /**
+     * Ensures the method equals() returns true:
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ensureEqualsReturnsTrueSuccessfully() throws Exception {
+    }
+
+    /**
+     * Ensures the method equals() returns false:
+     *
+     * @throws Exception
+     */
+    @Test
+    public void ensureEqualsReturnsFalseSuccessfully() throws Exception {
+    }
+
+    /**
+     * Ensures the method hashCode() returns the correct value
+     * @throws Exception
+     */
+    @Test
+    public void ensureHashCodeReturnsCorrectValue() throws Exception {
+    }
+
+}
