3f0ed499d6d3ea0b00c30e6ccc75219fafab36e8
Ana Rita <1161564@isep.ipp.pt>
2017-12-19 15:07:42 +0000

    [LP31718G38-85][Implementation] Changed method signature in interface to match the implementation of the method :: Exporter

--- src/main/java/lapr/project/utils/FileParser/Exporter.java
+++ src/main/java/lapr/project/utils/FileParser/Exporter.java
@@ -1,10 +1,12 @@
 package lapr.project.utils.FileParser;
 
+import java.io.IOException;
+
 /**
  * Handles exportation of an analysis result to a file
  */
 public interface Exporter {
 
-    void printDataFromAnalysis();
+    void printDataFromAnalysis() throws IOException;
 
 }
