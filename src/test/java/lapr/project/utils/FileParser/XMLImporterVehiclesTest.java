443d9db6248daa3243792b34015461ff7f5d78c6
pedrof111 <1160950@isep.ipp.pt>
2017-12-19 12:16:20 +0000

    [UnitTest] Fixed test class (not implemented yet)

--- src/test/java/lapr/project/utils/FileParser/XMLImporterVehiclesTest.java
+++ src/test/java/lapr/project/utils/FileParser/XMLImporterVehiclesTest.java
@@ -1,32 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package lapr.project.utils.FileParser;
 
 import java.util.ArrayList;
 import lapr.project.model.Project;
 import lapr.project.model.RoadNetwork.RoadNetwork;
-import lapr.project.utils.FileParser.XMLImporterVehicles;
-import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
- * XML Test
- * @author goncalo
+ *
+ * @author TOSHIBA
  */
 public class XMLImporterVehiclesTest {
-
-    private final String filename;
-
+    
     public XMLImporterVehiclesTest() {
-        this.filename="src/test/resources/TestSet02_Vehicles.xml";
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
     }
 
-
+    /**
+     * Test of importVehicles method, of class XMLImporterVehicles.
+     */
+    @Test
     public void testImportVehicles() {
         
         System.out.println("importVehicles");
         Project object = new Project("test","description",new RoadNetwork(),new ArrayList<>());
         XMLImporterVehicles instance = new XMLImporterVehicles();
         boolean expResult = true;
-        boolean result = instance.importVehicles(object, filename);
+        boolean result = instance.importVehicles(object, "src/test/resources/TestSet02_Vehicles.xml");
         assertEquals(expResult, result);
     }
-
+    
 }
