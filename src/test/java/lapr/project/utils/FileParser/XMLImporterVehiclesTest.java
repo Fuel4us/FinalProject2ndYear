440ec75ed9569696310a88d1fa2968ee31cdb032
pedrof111 <1160950@isep.ipp.pt>
2017-12-19 09:48:32 +0000

    [UnitTest] Created test class for XMLImporterVehicles

--- src/test/java/lapr/project/utils/FileParser/XMLImporterVehiclesTest.java
+++ src/test/java/lapr/project/utils/FileParser/XMLImporterVehiclesTest.java
@@ -1,39 +1,43 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package lapr.project.utils.FileParser;
-//
-//import lapr.project.model.Project;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-///**
-// *
-// * @author goncalo
-// */
-//public class XMLImporterVehiclesTest {
-//    
-//    public XMLImporterVehiclesTest() {
-//    }
-//    
-//    
-//
-//    /**
-//     * Test of importVehicles method, of class XMLImporterVehicles.
-//     */
-//    @Test
-//    public void testImportVehicles() {
-//        System.out.println("importVehicles");
-//        Project object = null;
-//        String filename = "";
-//        XMLImporterVehicles instance = new XMLImporterVehicles();
-//        boolean expResult = false;
-//        boolean result = instance.importVehicles(object, filename);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//    
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.utils.FileParser;
+
+import java.util.ArrayList;
+import java.util.List;
+import lapr.project.model.Project;
+import lapr.project.model.RoadNetwork.RoadNetwork;
+import lapr.project.model.Vehicle.Vehicle;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author goncalo
+ */
+public class XMLImporterVehiclesTest {
+    
+    private final String filename;
+    
+    public XMLImporterVehiclesTest() {
+        this.filename="src/test/resources/TestSet02_Vehicles.xml";
+    }
+    
+    
+
+    /**
+     * Test of importVehicles method, of class XMLImporterVehicles.
+     */
+    @Test
+    public void testImportVehicles() {
+        System.out.println("importVehicles");
+        Project object = new Project("test","description",new RoadNetwork(),new ArrayList<>());
+        XMLImporterVehicles instance = new XMLImporterVehicles();
+        boolean expResult = true;
+        boolean result = instance.importVehicles(object, filename);
+        assertEquals(expResult, result);
+    }
+    
+}
