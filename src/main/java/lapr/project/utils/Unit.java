2dfc7250e320a04151dfe65fb763965c4ae88353
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-19 19:01:29 +0000

    [Implementation] Renamed the constants and added the ROTATIONS_PER_MINUTE in the enum type Unit

--- src/main/java/lapr/project/utils/Unit.java
+++ src/main/java/lapr/project/utils/Unit.java
@@ -6,7 +6,8 @@ package lapr.project.utils;
  */
 public enum Unit {
     
-    KILOGRAM("kg"), GRAM("g"), KILOMETERSPERHOUR("km/h"), KILOMETER("km"), METER("m"), MILESPERHOUR("mp/h"), METERSPERSECOND("m/s");
+    KILOGRAM("kg"), GRAM("g"), KILOMETERS_PER_HOUR("km/h"), KILOMETER("km"), METER("m"), MILES_PER_HOUR("mp/h"), METERS_PER_SECOND("m/s"),
+    ROTATIONS_PER_MINUTE("rpm");
     private final String unit;
 
     Unit(String unit) {
