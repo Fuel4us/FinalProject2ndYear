ee95f5158fd5915e1d6d57acb1b38d1cfd839b07
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-19 19:18:19 +0000

    [Implementation] Updated the class Vehicle

--- src/main/java/lapr/project/model/Vehicle/Vehicle.java
+++ src/main/java/lapr/project/model/Vehicle/Vehicle.java
@@ -1,7 +1,12 @@
 package lapr.project.model.Vehicle;
 
+import lapr.project.model.RoadNetwork.RoadNetwork;
+import lapr.project.model.RoadNetwork.Segment;
+import lapr.project.utils.Measurable;
+import lapr.project.utils.Unit;
+
 import java.util.List;
-import lapr.project.utils.*;
+import java.util.Objects;
 
 /**
  * <p>
@@ -12,52 +17,58 @@ public class Vehicle {
 
     private String name;
     private String description;
-    
+
     private VehicleType type;
-    
+
     private int vehicleClass;
-    
+
     private Motorization motorization;
+
+    private MotorType motorType;
+
     private Fuel fuel;
+
     private Measurable mass;
     private Measurable load;
-    
+
     private float dragCoefficient;
     private float frontalArea;
     private float rollingReleaseCoefficient;
     private float wheelSize;
-    
+
     private List<VelocityLimit> velocityLimitList;
     private Energy energy;
 
     /**
-     * Constructor
-     *
-     * @param name
-     * @param description
-     * @param type
-     * @param vehicleClass
-     * @param motorType
-     * @param fuel
-     * @param mass
-     * @param load
-     * @param dragCoefficient
-     * @param frontalArea
-     * @param rollingReleaseCoefficient
-     * @param wheelSize
-     * @param velocityLimitList
-     * @param energy
+     * Creates a new vehicle
+     * @param name This vehicle's name
+     * @param description This vehicle's description
+     * @param type This vehicle's type
+     * @param vehicleClass This vehicle's class
+     * @param motorType This vehicle's motor type
+     * @param fuel This vehicle's fuel
+     * @param mass This vehicle's mass
+     * @param load This vehicle's load
+     * @param dragCoefficient This vehicle's drag coefficient
+     * @param frontalArea This vehicle's frontal area
+     * @param rollingReleaseCoefficient This vehicle's rolling release coefficient
+     * @param wheelSize This vehicle's wheel size
+     * @param velocityLimitList This vehicle's velocity limit list
+     * @param energy This vehicle's energy
      */
     public Vehicle(String name, String description, VehicleType type, int vehicleClass, MotorType motorType, Fuel fuel, Measurable mass, Measurable load, float dragCoefficient, Float frontalArea, float rollingReleaseCoefficient, Float wheelSize, List<VelocityLimit> velocityLimitList, Energy energy) {
         this.name = name;
         this.description = description;
         this.type = type;
         this.vehicleClass = vehicleClass;
+        this.motorType = motorType;
+
         if (motorType == MotorType.COMBUSTION) {
             motorization = new CombustionMotor();
         } else if (motorType == MotorType.NONCOMBUSTION) {
             motorization = new NonCombustionMotor();
         }
+
         this.fuel = fuel;
         this.mass = mass;
         this.load = load;
@@ -66,12 +77,7 @@ public class Vehicle {
         this.rollingReleaseCoefficient = rollingReleaseCoefficient;
         this.wheelSize = wheelSize;
         this.velocityLimitList = velocityLimitList;
-        this.energy = energy;
-    }
-    
-    @Override
-    public String toString(){
-        return String.format("%s - %s.", name,description);
+        this.energy = new Energy(energy);
     }
 
     /**
@@ -121,13 +127,89 @@ public class Vehicle {
         return null;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vehicle other = (Vehicle) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s.", name, description);
+    }
+
+//    /**
+//     * @param segment
+//     * @return
+//     */
+//    public double determineEnergyExpenditure(Segment segment) {
+//        //ToDo
+//        return 0;
+//    }
+//
+//    private Measurable calculateEngineSpeed(RoadNetwork roadNetwork, Segment segment) {
+//        double engineSpeed = 0;
+//
+//        double torque = determineIdealTorque(engineSpeed, segment, roadNetwork);
+//
+//        //ToDo
+////        double motorForce =
+//
+//        return new Measurable(engineSpeed, Unit.ROTATIONS_PER_MINUTE);
+//    }
+//
+//    private double determineIdealTorque(double engineSpeed, Segment segment, RoadNetwork roadNetwork) {
+//        //ToDo
+//        double maxVelocity = segment.calculateMinimumTimeInterval(roadNetwork, this);
+//        int i = energy.getGears().size() - 1;
+//
+//        engineSpeed = (maxVelocity * 60 * energy.getFinalDriveRatio() * energy.getGears().get(i).getRatio()) / (2 * Math.PI * (wheelSize / 2));
+//
+//        double torque = 0;
+//
+//        for (Regime regime : energy.getThrottles().get(0).getRegimes()) {
+//
+//            if (regime.getRpmHigh() >= engineSpeed && regime.getRpmLow() <= engineSpeed) {
+//                torque = regime.getTorque();
+//                break;
+//            }
+//        }
+//
+//        if (torque == 0) {
+//            maxVelocity -= maxVelocity * 0.02d;
+//            determineIdealTorque(engineSpeed, segment, roadNetwork);
+//        }
+//
+//        return torque;
+//    }
+
     /**
      * Indicates motor type
      * Assists in the instantiation of the correct motorization
      */
     public enum MotorType {
 
-        COMBUSTION, NONCOMBUSTION
+        COMBUSTION, NONCOMBUSTION;
 
     }
+
 }
