a8ae0210101485aad13e35d34aab2a15145354bb
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-20 09:01:14 +0000

    [Implementation] Created the class Physics that provides physics related utilities

---
+++ src/main/java/lapr/project/utils/Physics.java
@@ -0,0 +1,10 @@
+package lapr.project.utils;
+
+/**
+ * Provides physics related utilities
+ */
+public class Physics {
+
+    public static final Measurable GRAVITY_ACCELERATION = new Measurable(9.8, Unit.METERS_PER_SECOND_SQUARED);
+
+}
