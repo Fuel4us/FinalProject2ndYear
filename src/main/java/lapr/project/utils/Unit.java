582af7f2e4c90c9190bfd9fa45e90a227e4b3081
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-20 10:58:01 +0000

    [Implementation] Added the constant NEWTON_METER in the enum type Unit

--- src/main/java/lapr/project/utils/Unit.java
+++ src/main/java/lapr/project/utils/Unit.java
@@ -8,7 +8,7 @@ public enum Unit {
     
     KILOGRAM("kg"), GRAM("g"), KILOMETERS_PER_HOUR("km/h"), KILOMETER("km"), METER("m"), MILES_PER_HOUR("mp/h"), METERS_PER_SECOND("m/s"),
     ROTATIONS_PER_MINUTE("rpm"), DEGREE("º"), METERS_PER_SECOND_SQUARED("m/s^2"), METER_SQUARED("m^2"),
-    KILOGRAMS_PER_CUBIC_METER("kg/m^3");
+    KILOGRAMS_PER_CUBIC_METER("kg/m^3"), NEWTON_METER("Nm");
     private final String unit;
 
     Unit(String unit) {
