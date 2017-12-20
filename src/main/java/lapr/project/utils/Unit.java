e8a3d9c0e40d44fc500d4ab9e8d6f54d0c0d5b77
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-20 08:52:46 +0000

    [Implementation] Added the constant DEGREE in the enum type Unit

--- src/main/java/lapr/project/utils/Unit.java
+++ src/main/java/lapr/project/utils/Unit.java
@@ -7,7 +7,7 @@ package lapr.project.utils;
 public enum Unit {
     
     KILOGRAM("kg"), GRAM("g"), KILOMETERS_PER_HOUR("km/h"), KILOMETER("km"), METER("m"), MILES_PER_HOUR("mp/h"), METERS_PER_SECOND("m/s"),
-    ROTATIONS_PER_MINUTE("rpm");
+    ROTATIONS_PER_MINUTE("rpm"), DEGREE("º");
     private final String unit;
 
     Unit(String unit) {
