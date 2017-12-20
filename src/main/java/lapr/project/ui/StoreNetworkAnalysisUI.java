d1462458b751125f5d59e3d5640f357a296b0873
Catarina Antelo <1150379@isep.ipp.pt>
2017-12-20 14:16:34 +0000

    [UI]Connect UI to Network Analysis Controller

--- src/main/java/lapr/project/ui/StoreNetworkAnalysisUI.java
+++ src/main/java/lapr/project/ui/StoreNetworkAnalysisUI.java
@@ -5,18 +5,28 @@
  */
 package lapr.project.ui;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 import lapr.project.controller.NetworkAnalysisController;
 import lapr.project.model.Analysis;
 import lapr.project.model.Project;
 import lapr.project.utils.DataAccessLayer.DataBaseCommunicator;
+import lapr.project.utils.FileParser.ExportHTML;
 
 /**
  *
- * Provides user interface components for the display and storage of a network analysis
+ * Provides user interface components for the display and storage of a network
+ * analysis
  */
 public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
 
     private static final long serialVersionUID = 606009290497975171L;
+    private Project project;
+    private DataBaseCommunicator DBCom;
+    private Analysis generatedAnalysis;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel imgLateral;
     private javax.swing.JButton jButtonBack;
@@ -24,7 +34,7 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
     private javax.swing.JButton jButtonSave;
     private javax.swing.JLabel jLabelTitle;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField analysisResultsTextField;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JPanel orangeBorder;
     // End of variables declaration//GEN-END:variables
 
@@ -33,7 +43,7 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
     /**
      * Creates new form StoreNetworkAnalysisUI
      */
-    public StoreNetworkAnalysisUI() {
+    public StoreNetworkAnalysisUI() {       
         initComponents();
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -50,8 +60,6 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
 //        analysisResultsTextField.setText(generatedAnalysis.generateReport().toString());
 //        networkAnalysisController = new NetworkAnalysisController(currentProject, dbCom, generatedAnalysis);
 //    }
-
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -61,13 +69,12 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        InitializeUIElements initializer = new InitializeUIElements();
         jPanel1 = new javax.swing.JPanel();
         imgLateral = new javax.swing.JLabel();
         orangeBorder = new javax.swing.JPanel();
         jButtonGenerateFile = new javax.swing.JButton();
         jButtonBack = new javax.swing.JButton();
-        analysisResultsTextField = new javax.swing.JTextField();
+        jTextField1 = new javax.swing.JTextField();
         jLabelTitle = new javax.swing.JLabel();
         jButtonSave = new javax.swing.JButton();
 
@@ -90,28 +97,48 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
             .addGap(0, 0, Short.MAX_VALUE)
         );
 
+        jButtonGenerateFile.setBackground(new java.awt.Color(45, 46, 45));
+        jButtonGenerateFile.setFont(new java.awt.Font("Segoe UI Semibold", 0, 18)); // NOI18N
         jButtonGenerateFile.setForeground(new java.awt.Color(45, 46, 45));
-        initializer.initializeJButton(jButtonGenerateFile, Main.EIGHTEEN_SEGOE_FONT, "Generate HTML file", Main.DARK_GREY, new javax.swing.border.LineBorder(new java.awt.Color(250, 152, 60), 4, true));
-        jButtonGenerateFile.addActionListener(evt -> jButtonGenerateFileActionPerformed(evt));
+        jButtonGenerateFile.setText("Generate HTML file");
+        jButtonGenerateFile.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(250, 152, 60), 4, true));
+        jButtonGenerateFile.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonGenerateFileActionPerformed(evt);
+            }
+        });
 
+        jButtonBack.setBackground(new java.awt.Color(45, 46, 45));
+        jButtonBack.setFont(new java.awt.Font("Segoe UI Semibold", 0, 48)); // NOI18N
         jButtonBack.setForeground(new java.awt.Color(45, 46, 45));
-        initializer.initializeJButton(jButtonBack, Main.FORTY_EIGHT_SEGOE_FONT, "«", Main.DARK_GREY, new javax.swing.border.LineBorder(new java.awt.Color(250, 152, 60), 4, true));
-        jButtonBack.addActionListener(evt -> jButtonBackActionPerformed(evt));
+        jButtonBack.setText("«");
+        jButtonBack.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(250, 152, 60), 4, true));
+        jButtonBack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonBackActionPerformed(evt);
+            }
+        });
 
-        analysisResultsTextField.setBackground(new java.awt.Color(97, 122, 133));
-        analysisResultsTextField.setForeground(new java.awt.Color(204, 204, 204));
-        analysisResultsTextField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        analysisResultsTextField.setText("<sample path>");
-        analysisResultsTextField.setEditable(false);
-        analysisResultsTextField.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(155, 177, 189), 2));
+        jTextField1.setBackground(new java.awt.Color(97, 122, 133));
+        jTextField1.setForeground(new java.awt.Color(204, 204, 204));
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField1.setText("<sample path>");
+        jTextField1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(155, 177, 189), 2));
 
         jLabelTitle.setFont(new java.awt.Font("SF Movie Poster", 0, 48)); // NOI18N
         jLabelTitle.setForeground(new java.awt.Color(155, 177, 189));
         jLabelTitle.setText("Network analysis result");
 
+        jButtonSave.setBackground(new java.awt.Color(45, 46, 45));
+        jButtonSave.setFont(new java.awt.Font("Segoe UI Semibold", 0, 18)); // NOI18N
         jButtonSave.setForeground(new java.awt.Color(45, 46, 45));
-        initializer.initializeJButton(jButtonSave, Main.EIGHTEEN_SEGOE_FONT, "Save results", Main.DARK_GREY, new javax.swing.border.LineBorder(new java.awt.Color(250, 152, 60), 4, true));
-        jButtonSave.addActionListener(this::jButtonSaveActionPerformed);
+        jButtonSave.setText("Save results");
+        jButtonSave.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(250, 152, 60), 4, true));
+        jButtonSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSaveActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -133,7 +160,7 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
                             .addComponent(jButtonGenerateFile, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButtonSave, javax.swing.GroupLayout.PREFERRED_SIZE, 215, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addComponent(analysisResultsTextField, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 460, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 460, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(63, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -146,7 +173,7 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabelTitle)
                 .addGap(18, 18, 18)
-                .addComponent(analysisResultsTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButtonBack, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -167,16 +194,24 @@ public class StoreNetworkAnalysisUI extends javax.swing.JFrame {
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
-        setResizable(false);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonGenerateFileActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonGenerateFileActionPerformed
-        // TODO add your handling code here:
+        JFileChooser fileChooser = new JFileChooser();
+        File file = fileChooser.getSelectedFile();
+        NetworkAnalysisController expAnalysis = new NetworkAnalysisController(project, DBCom, generatedAnalysis);
+        try {
+            expAnalysis.exportData(file);
+        } catch (IOException ex) {
+            Logger.getLogger(StoreNetworkAnalysisUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_jButtonGenerateFileActionPerformed
 
     private void jButtonBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBackActionPerformed
-        // TODO add your handling code here:
+        BestPathUI.main(null);
+        dispose();
     }//GEN-LAST:event_jButtonBackActionPerformed
 
     private void jButtonSaveActionPerformed(java.awt.event.ActionEvent evt) {
