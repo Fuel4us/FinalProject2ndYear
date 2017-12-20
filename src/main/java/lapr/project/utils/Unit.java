bddbf757f736436fa0a60a1589e00ae50cfb5f98
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-20 09:00:41 +0000

    [Implementation] Added the constant METERS_PER_SECOND_SQUARED in the enum type Unit

--- src/main/java/lapr/project/utils/Unit.java
+++ src/main/java/lapr/project/utils/Unit.java
@@ -7,7 +7,7 @@ package lapr.project.utils;
 public enum Unit {
     
     KILOGRAM("kg"), GRAM("g"), KILOMETERS_PER_HOUR("km/h"), KILOMETER("km"), METER("m"), MILES_PER_HOUR("mp/h"), METERS_PER_SECOND("m/s"),
-    ROTATIONS_PER_MINUTE("rpm"), DEGREE("º");
+    ROTATIONS_PER_MINUTE("rpm"), DEGREE("º"), METERS_PER_SECOND_SQUARED("m/s^2");
     private final String unit;
 
     Unit(String unit) {
