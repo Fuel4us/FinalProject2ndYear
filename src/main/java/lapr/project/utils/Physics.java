ac187c775c22c9e991aa8947bcb3b0dde625929e
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-20 09:37:05 +0000

    [Implementation] Added the constant AIR_DENSITY in the class Physics

--- src/main/java/lapr/project/utils/Physics.java
+++ src/main/java/lapr/project/utils/Physics.java
@@ -6,5 +6,6 @@ package lapr.project.utils;
 public class Physics {
 
     public static final Measurable GRAVITY_ACCELERATION = new Measurable(9.8, Unit.METERS_PER_SECOND_SQUARED);
+    public static final Measurable AIR_DENSITY = new Measurable(1.225, Unit.KILOGRAMS_PER_CUBIC_METER);
 
 }
