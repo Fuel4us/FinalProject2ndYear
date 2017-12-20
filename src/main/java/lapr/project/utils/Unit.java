ea7523a90dd829f79bde3e65bc3f25fc12c02a78
Bruno Pinto [1161063] <1161063@isep.ipp.pt>
2017-12-20 09:33:25 +0000

    [Implementation] Added the constant KILOGRAMS_PER_CUBIC_METER in the enum type Unit

--- src/main/java/lapr/project/utils/Unit.java
+++ src/main/java/lapr/project/utils/Unit.java
@@ -7,7 +7,8 @@ package lapr.project.utils;
 public enum Unit {
     
     KILOGRAM("kg"), GRAM("g"), KILOMETERS_PER_HOUR("km/h"), KILOMETER("km"), METER("m"), MILES_PER_HOUR("mp/h"), METERS_PER_SECOND("m/s"),
-    ROTATIONS_PER_MINUTE("rpm"), DEGREE("º"), METERS_PER_SECOND_SQUARED("m/s^2"), METER_SQUARED("m^2");
+    ROTATIONS_PER_MINUTE("rpm"), DEGREE("º"), METERS_PER_SECOND_SQUARED("m/s^2"), METER_SQUARED("m^2"),
+    KILOGRAMS_PER_CUBIC_METER("kg/m^3");
     private final String unit;
 
     Unit(String unit) {
