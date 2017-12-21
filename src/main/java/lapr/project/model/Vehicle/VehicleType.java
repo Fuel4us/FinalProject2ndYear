63268f0da569357c673a69982f22fa41b2d0a6fe
Gonçalo Fonseca <goncalo7bfc@gmail.com>
2017-12-21 15:55:40 +0000

    [Fix] Delete JAXB mentions

--- src/main/java/lapr/project/model/Vehicle/VehicleType.java
+++ src/main/java/lapr/project/model/Vehicle/VehicleType.java
@@ -1,11 +1,8 @@
 package lapr.project.model.Vehicle;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
 /**
  * Defines discrete values for vehicle designations
  */
-@XmlRootElement
 public enum VehicleType {
     Truck,
     Tractor,
