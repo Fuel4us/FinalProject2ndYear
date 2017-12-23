dc55da0d920af4c269adb2ef8e14decb7dde1cd5
Gonçalo Fonseca <goncalo7bfc@gmail.com>
2017-12-23 13:53:09 +0000

    [UnitTest] other test to unit

--- src/test/java/lapr/project/utils/UnitTest.java
+++ src/test/java/lapr/project/utils/UnitTest.java
@@ -1,10 +1,5 @@
 package lapr.project.utils;
 
-import com.google.gson.stream.JsonToken;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
