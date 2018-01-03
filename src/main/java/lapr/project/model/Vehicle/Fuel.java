1a74d858646577de696c86ad1282eb44aa983964
Pedro <1161028@isep.ipp.pt>
2018-01-03 13:32:03 +0000

    [Implementation] Added constants for algorithm names :: PathAlgorithm

--- src/main/java/lapr/project/model/Vehicle/Fuel.java
+++ src/main/java/lapr/project/model/Vehicle/Fuel.java
@@ -3,9 +3,12 @@ package lapr.project.model.Vehicle;
 import lapr.project.utils.Measurable;
 import lapr.project.utils.Unit;
 
+import javax.persistence.Embeddable;
+
 /**
  * Type of fuel that the car uses
  */
+@Embeddable
 public enum Fuel {
 
     Diesel(new Measurable(48, Unit.KILOJOULE_PER_GRAM)), Gasoline(new Measurable(44.4, Unit.KILOJOULE_PER_GRAM)),
@@ -18,6 +21,7 @@ public enum Fuel {
      * @param specificEnergy the specific energy
      */
     Fuel(Measurable specificEnergy) {
+
         this.specificEnergy = specificEnergy;
     }
 
