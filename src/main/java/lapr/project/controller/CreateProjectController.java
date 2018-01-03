9fb1208721fc493684926b3af4f41ccb300ed63f
pedrof111 <1160950@isep.ipp.pt>
2018-01-03 11:42:37 +0000

    [Implementation][P02] US code fixed and fully functional
    US code fixed and fully functional although the application doesnt stoer the project yet

--- src/main/java/lapr/project/controller/CreateProjectController.java
+++ src/main/java/lapr/project/controller/CreateProjectController.java
@@ -13,24 +13,25 @@ import lapr.project.utils.FileParser.ImportFile;
 import lapr.project.utils.FileParser.XMLImporterRoads;
 import lapr.project.utils.FileParser.XMLImporterVehicles;
 
-
 /**
  * Class that represents the Controller for the UC2 - Create Project
  */
 public class CreateProjectController {
 
     private DataBaseCommunicator dbCom;
-    private Project project;
+    private static Project project;
 
-    private List<Vehicle> vehicles;
-    private RoadNetwork roadNetwork;
+    private File roadsFile;
+    private File vehiclesFile;
 
     /**
      * Full constructor for the class CreateProjectController
      */
     public CreateProjectController(DataBaseCommunicator dbCom) {
         this.dbCom = dbCom;
-        this.project = new Project();
+        project = new Project();
+        this.roadsFile = null;
+        this.vehiclesFile = null;
     }
 
     public Project getProject() {
@@ -40,57 +41,27 @@ public class CreateProjectController {
     public DataBaseCommunicator getDbCom() {
         return dbCom;
     }
-    
-     /**
-     * Validate the project file integrity
-     *
-     * @param filename
-     * @param type
-     * @return
-     */
-    public boolean validate(String filename, String type) {
-        File file = new File(filename);
-        if (file.exists()) {
-            return true;
-        }
-        return false;
-    }
-    
-    public boolean newProject(String fileType, String inputType, String fileName) {
-        
-        ImportFile file = new ImportFile();
-        FileParser type;
-        FileParser type2;
-        
-        switch(inputType) {
-            case ".xml":
-                type = new XMLImporterVehicles();
-                type2 = new XMLImporterRoads(null); // String cannot be converted to File
-                break;
-            default:
-                return false;
-        }
-        
-        if(fileType.equalsIgnoreCase("vehicles")) {
-            file.importVehicles(type, project, fileName);
-        } else {
-            System.out.println("Didn't find");
-        }
-        if(fileType.equalsIgnoreCase("network")) {
-            file.importNetwork(type2, project, fileName);
-        } else {
-            System.out.println("Didn't find");
-        }
-        
-        return false;
-        
+
+    public void setRoads(File roads) {
+        this.roadsFile = roads;
     }
 
-    public void setProject(int id, String name, String description) {
-        project.setId(id);
-        project.setName(name);
-        project.setDescription(description);
+    public void setVehicles(File vehicles) {
+        this.vehiclesFile = vehicles;
     }
-    
-    
+
+    public File getRoadsFile() {
+        return roadsFile;
+    }
+
+    public File getVehiclesFile() {
+        return vehiclesFile;
+    }
+
+    public void createProject(String name, String description) throws Exception {
+        XMLImporterRoads roadImporter = new XMLImporterRoads(roadsFile);
+        project = new Project(name, description, roadImporter.importNetwork(), null);
+        new XMLImporterVehicles().importVehicles(project, vehiclesFile.getAbsolutePath());
+    }
+
 }
