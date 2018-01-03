48d6f8b458fe9b9a23929e6a22fcd01adc0985ec
Ana Rita <1161564@isep.ipp.pt>
2018-01-03 10:23:03 +0000

    [LP31718G38-168][Implementation] Changed return type of method storeProject to boolean :: ProjectDAO

--- src/main/java/lapr/project/utils/DataAccessLayer/Abstraction/ProjectDAO.java
+++ src/main/java/lapr/project/utils/DataAccessLayer/Abstraction/ProjectDAO.java
@@ -2,7 +2,6 @@ package lapr.project.utils.DataAccessLayer.Abstraction;
 
 import lapr.project.model.Project;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -21,5 +20,5 @@ public interface ProjectDAO extends DataAccessObject {
      * Stores instance of {@link Project} in the database
      * @param project instance of {@link Project}
      */
-    void storeProject(Project project);
+    boolean storeProject(Project project);
 }
