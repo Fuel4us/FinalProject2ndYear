e8e9d66d55b48d53ef56e3b9a636484b12ee5a6b
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-03 09:08:50 +0000

    [Implementation] Removed the XMLElements in the class Road

--- src/main/java/lapr/project/model/RoadNetwork/Road.java
+++ src/main/java/lapr/project/model/RoadNetwork/Road.java
@@ -2,27 +2,20 @@ package lapr.project.model.RoadNetwork;
 
 import lapr.project.model.Vehicle.Vehicle;
 
-import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Represents the roads
  */
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.FIELD)
 public class Road {
 
-    @XmlAttribute
     private String id;
 
-    @XmlElement(name = "road_name")
     private String name;
 
-    @XmlElement
     private String typology;
-    @XmlElementWrapper(name = "toll_fare")
-    @XmlElement(name = "class")
+
     private List<Double> tollFare;
 
     /**
