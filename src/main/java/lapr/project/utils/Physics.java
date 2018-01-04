be0fef2fef9218bee3e75244048d661976143204
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-04 15:57:32 +0000

    [Implementation] Corrected the method calculateQuadraticFormula in the class Physics to return both values in the quadratic formula

--- src/main/java/lapr/project/utils/Physics.java
+++ src/main/java/lapr/project/utils/Physics.java
@@ -11,20 +11,23 @@ public class Physics {
     public static final double KILOMETERS_METERS_CONVERSION_RATIO = 0.001;
 
     /**
-     * Calculates the quadratic formula with the values a, b and c and returns one of the values that should
-     * be returned (when the signal is '+' (boolean -> true) and when the signal is '-' (boolean -> false))
+     * Calculates the quadratic formula with the values a, b and c and returns both values stored in an array
      * @param a the first value
      * @param b the second value
      * @param c the third value
-     * @param plus the boolean that indicates whether the operation is '+' (true) or '-' (false)
-     * @return the value given by the operation of the quadratic formula
+     * @return an array storing the values given by the operation of the quadratic formula
      */
-    public static double calculateQuadraticFormula(double a, double b, double c, boolean plus) {
-        if (plus) {
-            return (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
-        } else {
-            return (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    public static double[] calculateQuadraticFormula(double a, double b, double c) {
+        double discriminant = Math.pow(b, 2) - 4 * a * c;
+
+        if (discriminant < 0) {
+            return null;
         }
+        
+        double value1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double value2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        return new double[]{value1, value2};
     }
 
 }
