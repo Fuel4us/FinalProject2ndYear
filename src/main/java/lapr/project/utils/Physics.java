df5ca7a47f06b28c05373e00b3f9e2e7b3abe929
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-04 15:35:58 +0000

    [Implementation] Added the method calculateQuadraticFormula in the class Physics

--- src/main/java/lapr/project/utils/Physics.java
+++ src/main/java/lapr/project/utils/Physics.java
@@ -10,4 +10,21 @@ public class Physics {
     public static final double KILOMETERS_PER_HOUR_METERS_PER_SECOND_CONVERSION_RATIO = 3.6;
     public static final double KILOMETERS_METERS_CONVERSION_RATIO = 0.001;
 
+    /**
+     * Calculates the quadratic formula with the values a, b and c and returns one of the values that should
+     * be returned (when the signal is '+' (boolean -> true) and when the signal is '-' (boolean -> false))
+     * @param a the first value
+     * @param b the second value
+     * @param c the third value
+     * @param plus the boolean that indicates whether the operation is '+' (true) or '-' (false)
+     * @return the value given by the operation of the quadratic formula
+     */
+    public static double calculateQuadraticFormula(double a, double b, double c, boolean plus) {
+        if (plus) {
+            return (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        } else {
+            return (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        }
+    }
+
 }
