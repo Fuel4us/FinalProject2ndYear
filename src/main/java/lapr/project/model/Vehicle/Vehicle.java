dd7f066e73da06e1eaa6f24afc60898252fce75a
Ana Rita <1161564@isep.ipp.pt>
2018-01-05 09:49:38 +0000

    [LP31718G38-179][Implementation] Added store of vehicle name in method storeVehicleInformation :: Vehicle

--- src/main/java/lapr/project/model/Vehicle/Vehicle.java
+++ src/main/java/lapr/project/model/Vehicle/Vehicle.java
@@ -311,6 +311,7 @@ public class Vehicle {
     }
 
     public void storeVehicleInformation(CallableStatement storeVehicleInfoProcedure) throws SQLException {
+        storeVehicleInfoProcedure.setString("name", name);
         storeVehicleInfoProcedure.setString("vehicleType", type.name());
         storeVehicleInfoProcedure.setInt("vehicleTollClass", vehicleClass);
         storeVehicleInfoProcedure.setString("motorType", motorType.name());
