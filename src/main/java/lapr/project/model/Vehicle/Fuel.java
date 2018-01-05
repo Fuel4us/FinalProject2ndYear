44e755d33b5632d23d32c3759fd0c32f672eb7cf
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-05 10:25:46 +0000

    Merged into master branch

--- src/main/java/lapr/project/model/Vehicle/Fuel.java
+++ src/main/java/lapr/project/model/Vehicle/Fuel.java
@@ -3,12 +3,9 @@ package lapr.project.model.Vehicle;
 import lapr.project.utils.Measurable;
 import lapr.project.utils.Unit;
 
-import javax.persistence.Embeddable;
-
 /**
  * Type of fuel that the car uses
  */
-@Embeddable
 public enum Fuel {
 
     Diesel(new Measurable(48, Unit.KILOJOULE_PER_GRAM)), Gasoline(new Measurable(44.4, Unit.KILOJOULE_PER_GRAM)),
