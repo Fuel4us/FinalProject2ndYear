3df99e0c7a3313da294fb8c9264c4bab9ab2606a
Pedro <1161028@isep.ipp.pt>
2018-01-07 12:09:54 +0000

    [UnitTest][LP31718G38-181] Added test case for dynamicWeigthSPFinder :: GraphAlgorithmsTest

--- src/test/java/lapr/project/utils/Graph/GraphAlgorithmsTest.java
+++ src/test/java/lapr/project/utils/Graph/GraphAlgorithmsTest.java
@@ -1,23 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package lapr.project.utils.Graph;
 
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import org.junit.AfterClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.function.Function;
+
+import static org.junit.Assert.assertTrue;
 
 public class GraphAlgorithmsTest {
 
-    Graph<String, String> completeMap = new Graph<>(false);
+    private Graph<String, String> completeMap = new Graph<>(false);
 
     public GraphAlgorithmsTest() {
     }
@@ -49,14 +43,6 @@ public class GraphAlgorithmsTest {
         completeMap.insertEdge("Aveiro", "Leiria", "A17", 120);
         completeMap.insertEdge("Leiria", "Lisboa", "A8", 150);
 
-//        incompleteMap = completeMap.clone();
-//        completeMap.insertEdge("Aveiro","Viseu","A25",85);
-//        completeMap.insertEdge("Leiria","Castelo Branco","A23",170);
-//        completeMap.insertEdge("Lisboa","Faro","A2",280);
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
     }
 
     /**
@@ -64,7 +50,6 @@ public class GraphAlgorithmsTest {
      */
     @Test
     public void testBreadthFirstSearch() {
-        System.out.println("Test BreadthFirstSearch");
 
         assertTrue("Should be null if vertex does not exist", GraphAlgorithms.BreadthFirstSearch(completeMap, "LX") == null);
 
@@ -86,7 +71,6 @@ public class GraphAlgorithmsTest {
      */
     @Test
     public void testDepthFirstSearch() {
-        System.out.println("Test of DepthFirstSearch");
 
         LinkedList<String> path;
 
@@ -116,9 +100,8 @@ public class GraphAlgorithmsTest {
      */
     @Test
     public void testShortestPath() {
-        System.out.println("Test of shortest path");
 
-        LinkedList<String> shortPath = new LinkedList<String>();
+        LinkedList<String> shortPath = new LinkedList<>();
         double lenpath = 0;
         lenpath = GraphAlgorithms.shortestPath(completeMap, "Porto", "LX", shortPath);
         assertTrue("Length path should be 0 if vertex does not exist", shortPath.size() == 0);
@@ -164,7 +147,7 @@ public class GraphAlgorithmsTest {
         assertTrue("then Guarda", it.next().compareTo("Guarda") == 0);
         assertTrue("then Castelo Branco", it.next().compareTo("Castelo Branco") == 0);
 
-        //Changing Edge: Aveiro-Viseu with Edge: Leiria-C.Branco 
+        //Changing Edge: Aveiro-Viseu with Edge: Leiria-C.Branco
         //should change shortest path between Porto and Castelo Branco
         completeMap.removeEdge("Aveiro", "Viseu");
         completeMap.insertEdge("Leiria", "Castelo Branco", "A23", 170);
@@ -181,4 +164,140 @@ public class GraphAlgorithmsTest {
         assertTrue("then Castelo Branco", it.next().compareTo("Castelo Branco") == 0);
 
     }
+
+    /**
+     * Method : cumulative dijkstra (invoked through non private shortestPath overload)
+     */
+    @Test
+    public void ensureCumulativeDijkstraOutputsCorrectPathAndPathCost() throws Exception {
+
+        //<editor-fold desc="Initialization">
+        double seed = 2d;
+        Function<Double, Double> cumulativeAttributeExtractor = x -> x * 3;
+
+        Graph<String, TestEdge> graph = new Graph<>(true);
+        String a = "A";
+        String b = "B";
+        String c = "C";
+        String d = "D";
+
+        graph.insertVertex(a);
+        graph.insertVertex(b);
+        graph.insertVertex(c);
+        graph.insertVertex(d);
+
+        TestEdge edge0 = new TestEdge();
+        TestEdge edge1 = new TestEdge();
+        TestEdge edge2 = new TestEdge();
+        TestEdge edge3 = new TestEdge();
+        TestEdge edge4 = new TestEdge();
+        TestEdge edge5 = new TestEdge();
+        //</editor-fold>
+
+        //the weight is defined via the weight extractor, so the value of weight passed in insertEdge is irrelevant
+
+        //path one : a-b-c-d, edges : 0-1-2
+        graph.insertEdge(a, b, edge0, 1);
+        graph.insertEdge(c, d, edge1, 1);
+        graph.insertEdge(b, c, edge2, 1);
+
+        //cumulative transformation (lambda x, f(x)) -> f(f(x))), costs of new edges depend on previous edge
+        double edge0Cost = edge0.computeUniqueValueSeedEdgeProperties(seed);
+        double edge2Cost = edge2.computeUniqueValueSeedEdgeProperties(cumulativeAttributeExtractor.apply(edge0Cost));
+        double edge1Cost = edge1.computeUniqueValueSeedEdgeProperties(cumulativeAttributeExtractor.apply(edge2Cost));
+
+        double pathOneCost = edge0Cost + edge1Cost + edge2Cost;
+        List<String> pathOne = Arrays.asList(a, b, c, d);
+
+        //path two: a-d, edges : 3
+        graph.insertEdge(a, d, edge3, 1);
+
+        double pathTwoCost = edge3.computeUniqueValueSeedEdgeProperties(seed);
+        List<String> pathTwo = Arrays.asList(a, d);
+
+        //path 3 : a-c-d, edges : 4-1 (the graph is directed)
+        graph.insertEdge(a, c, edge4, 1);
+        graph.insertEdge(d, c, edge5, 1);
+
+        //cumulative transformation (lambda x, f(x)) -> f(f(x))), costs of new edges depend on previous edge
+        double edge4Cost = edge4.computeUniqueValueSeedEdgeProperties(seed);
+        edge1Cost = edge1.computeUniqueValueSeedEdgeProperties(cumulativeAttributeExtractor.apply(edge4Cost));
+
+        double pathThreeCost = edge4Cost + edge1Cost;
+        List<String> pathThree = Arrays.asList(a, c, d);
+
+        //Cost function
+        //lambda x lambda g lambda f -> sum f(g(x)) for x in numNodes - 1
+        // \xgf -> sum f(g(x)) for x in numPathEdges where numPathEdges = numNodes - 1
+        LinkedList<String> shortestPath = new LinkedList<>();
+        double totalCost = GraphAlgorithms.shortestPath(graph, a, d, shortestPath,
+                //cumulative applier (transformation function)
+                (edge, argument) -> edge.getElement().computeUniqueValueSeedEdgeProperties(argument),
+                //weigth extractor - defines how weight is interpreted from the output of the transformation function
+                value -> value,
+                //initial value for the first edge, upon which the weigth of all other edges depends
+                seed,
+                //defines how the argument for the next call is interpreted/obtained/extracted from the output of the transformation function
+                cumulativeAttributeExtractor);
+
+        double min1 = Math.min(pathOneCost, pathTwoCost);
+        double min2 = Math.min(pathTwoCost, pathThreeCost);
+        double leastCost = Math.min(min1, min2);
+
+        assert totalCost == leastCost;
+
+        if (leastCost == pathOneCost) {
+            assert shortestPath.equals(pathOne);
+        } else if (leastCost == pathTwoCost) {
+            assert shortestPath.equals(pathTwo);
+        } else if (leastCost == pathThreeCost) {
+            assert shortestPath.equals(pathThree);
+        } else throw new AssertionError("Least cost path not found!");
+
+    }
+
+    /**
+     * Edge subclass for test purposes only
+     */
+    private static class TestEdge extends Edge {
+
+        private int id;
+
+        private static int numInstances = 0;
+
+        TestEdge() {
+            this.id = numInstances;
+            numInstances++;
+        }
+
+        /**
+         * Simulate different outputs for different instances, even if the same arguments are provided
+         * @param seed a seed value
+         * @return the generated value
+         */
+        double computeUniqueValueSeedEdgeProperties(Double seed) {
+            return seed * (id + 1);
+        }
+
+        @Override
+        public String toString() {
+
+            return "edge" + id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+            TestEdge testEdge = (TestEdge) o;
+            return id == testEdge.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), id);
+        }
+    }
+
 }
