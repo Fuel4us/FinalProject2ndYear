e173a975a691c0a3f5c03145dbef998508ca0d11
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-08 09:24:44 +0000

    [UnitTest] Added the test for the method determineInitialVelocity in the class Vehicle

--- src/test/java/lapr/project/model/Vehicle/VehicleTest.java
+++ src/test/java/lapr/project/model/Vehicle/VehicleTest.java
@@ -6,6 +6,8 @@
 package lapr.project.model.Vehicle;
 
 import java.util.ArrayList;
+import java.util.List;
+
 import lapr.project.model.Project;
 import lapr.project.model.RoadNetwork.RoadNetwork;
 import lapr.project.model.RoadNetwork.Segment;
@@ -102,6 +104,62 @@ public class VehicleTest {
         boolean result = instance.equals(testVehicle1);
         assertEquals(expResult, result);
         assertTrue(instance.equals(testVehicle2));
+    }
+
+    /**
+     * Ensures the method determineInitialVelocity calculates correctly the initial velocity of the vehicle
+     */
+    @Test
+    public void ensureInitialVelocityIsCorrect() {
+
+        List<VelocityLimit> velocityLimitListTest = new ArrayList<>();
+        velocityLimitListTest.add(new VelocityLimit("Highway", new Measurable(110, Unit.KILOMETERS_PER_HOUR)));
+        velocityLimitListTest.add(new VelocityLimit("Road", new Measurable(80, Unit.KILOMETERS_PER_HOUR)));
+
+        List<Gears> gearsTest = new ArrayList<>();
+        gearsTest.add(new Gears(1, 4.5f));
+        gearsTest.add(new Gears(2, 3.5f));
+        gearsTest.add(new Gears(3, 2.7f));
+        gearsTest.add(new Gears(4, 1.6f));
+        gearsTest.add(new Gears(5, 1.2f));
+        gearsTest.add(new Gears(6, 0.9f));
+
+        List<Regime> regimes25Test = new ArrayList<>();
+        regimes25Test.add(new Regime(115, 125, 900, 1499, 500));
+        regimes25Test.add(new Regime(125, 120, 1500, 2499, 450));
+        regimes25Test.add(new Regime(120, 105, 2500, 3499, 520));
+        regimes25Test.add(new Regime(105, 90, 3500, 4499, 550));
+        regimes25Test.add(new Regime(90, 80, 4500, 5500, 650));
+
+        List<Regime> regimes50Test = new ArrayList<>();
+        regimes50Test.add(new Regime(185, 195, 900, 1499, 380));
+        regimes50Test.add(new Regime(195, 190, 1500, 2499, 350));
+        regimes50Test.add(new Regime(190, 180, 2500, 3499, 360));
+        regimes50Test.add(new Regime(180, 150, 3500, 4499, 400));
+        regimes50Test.add(new Regime(150, 135, 4500, 5500, 520));
+
+        List<Regime> regimes100Test = new ArrayList<>();
+        regimes100Test.add(new Regime(305, 325, 900, 1499, 380));
+        regimes100Test.add(new Regime(325, 315, 1500, 2499, 350));
+        regimes100Test.add(new Regime(315, 290, 2500, 3499, 360));
+        regimes100Test.add(new Regime(290, 220, 3500, 4499, 400));
+        regimes100Test.add(new Regime(220, 205, 4500, 5500, 520));
+
+        List<Throttle> throttlesTest = new ArrayList<>();
+        throttlesTest.add(new Throttle(25, regimes25Test));
+        throttlesTest.add(new Throttle(50, regimes50Test));
+        throttlesTest.add(new Throttle(100, regimes100Test));
+
+        Vehicle vehicleTest = new Vehicle("Toyota", "Vehicle 1", VehicleType.Car, 1, Vehicle.MotorType.COMBUSTION, Fuel.Diesel,
+                new Measurable(1500, Unit.KILOGRAM), new Measurable(7500, Unit.KILOGRAM), 0.320f,
+                new Measurable(1.9, Unit.METER_SQUARED), 0.01f, new Measurable(0.6, Unit.METER),
+                velocityLimitListTest, new Energy(900, 5500, 4f, gearsTest, throttlesTest));
+
+        Measurable expected = new Measurable(5.654866, Unit.KILOMETERS_PER_HOUR);
+        Measurable result = vehicleTest.determineInitialVelocity();
+
+        assertEquals(expected.getQuantity(), result.getQuantity(), 0.00001);
+
     }
 
 //    /**
