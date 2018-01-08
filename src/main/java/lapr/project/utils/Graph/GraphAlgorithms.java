db184997ba06982490ee61ba1a1c4f0170eb2af4
Pedro <1161028@isep.ipp.pt>
2018-01-08 10:33:49 +0000

    [JavaDoc] Updated class documentation :: GraphAlgorithms

--- src/main/java/lapr/project/utils/Graph/GraphAlgorithms.java
+++ src/main/java/lapr/project/utils/Graph/GraphAlgorithms.java
@@ -325,19 +325,20 @@ public class GraphAlgorithms {
     }
 
     /**
+     * <h1>Dynamic Weight Shortest Path Finder Algorithm</h1>
      * <p>
      * Computes shortest-path distance from a source vertex to all reachable
-     * vertices of a graph g with non negative edge weights
-     * This implementation uses Dijkstra's algorithm, and applies cumulative dependency between outputs of previous iterations.
+     * vertices of a <em>dynamic weight graph</em> with non negative edge weights.
+     * A <em>dynamic weight graph</em> is a graph where there are updates to the values of the weight according to previously visited edges.
+     * This implementation is based off of Dijkstra's algorithm but applies cumulative dependency between outputs of previous iterations.
+     * Three functional interfaces may be implemented to customize behaviour, allowing the use of this method for several different cases,
+     * as long as the contracts for these interfaces are satisfied.
      * <br>
      * <br>
      * <em>This means that this algorithm can be used to compute the shortest path, whenever the weight of an edge depends upon the weight of the previous visited edge.</em>
      * <br>
      * <br>
      * It abstracts from implementation details since it is coupled to functional interfaces via the use of functional programming oriented techniques.
-     * <br>
-     * <br>
-     * However, effective use of this method requires awareness of the envisioned use for these functions.
      * <br>
      * <br>
      * The algorithm invokes a given function of type {@link BiFunction} named {@code cumulativeApplier}
@@ -351,18 +352,30 @@ public class GraphAlgorithms {
      * of the previous call is passed on as an argument to the {@code cumulativeApplier} so that a transformation can take place iteratively,
      * which is to mean, simply, <em>that the weight of an edge is dependent upon the weight of the previous edge</em>, and a specific type,
      * which can be resolved into an argument, can be passed on to be used to the next iteration, in the calculation of the weight of the next edge being visited.
-     * The function that transforms the output of the method called by the implementation of {@code cumulativeApplier} is the functional interface instance ({@link Function}) {@code weightExtractor}
+     * The function that resolves the output of the method called by the implementation of {@code cumulativeApplier} into the definition of weigth to be taken into account
+     * is the functional interface instance ({@link Function}) {@code weightExtractor}
      * <p>
      * The starting value for the iterative application performed by {@code cumulativeApplier} is given by a {@code seed},
      * which must be of the same type as the one returned by the implementation of {@code cumulativeAttributeExtractor}, since this is the one that will be used for every edge after the first.
      * </p>
-     * @param g Graph instance
-     * @param vOrig Vertex that will be the source of the path
+     * <br>
+     * <p>
+     * The cost of the path can be summarized as the sum of the composition of these functions
+     * <p>
+     * <h1>Cost function</h1>
+     * (Syntax based off of lambda calculus / Haskell)
+     * <br>
+     * lambda x lambda g lambda f -> sum f(g(x)) for x in numNodes - 1
+     * <br>
+     * \xgf -> sum f(g(x)) for x in numPathEdges where numPathEdges = numNodes - 1
+     * <p>
+     * </p>
      * @param cumulativeApplier Defines a function wherein input can be the result of a previous call to this function, given a seed
      * @param seed The starting value for the transformation/cumulative function
      * @param cumulativeAttributeExtractor Extracts the information of the input to pass on to the next call by invoking a method on the return type of the function
      * This can be the identity function (x -> x) in case return type is the same as the transformed input.
      * @param weightExtractor Extracts the definition of weigth of an edge to be used from the output of the method called by the cumulativeApplier implementation (lambda/anonymous class/method reference)
+     * @author Pedro Lopes
      */
     public static <I, S, E, P> double shortestPath(Graph<I, S> g, I vOrig, I vDest, LinkedList<I> shortPath,
                                                    BiFunction<Edge<I, S>, P, E> cumulativeApplier,
