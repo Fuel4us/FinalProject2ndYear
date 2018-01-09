05ceebe4f98ea7d08d982e66bbc89b28dfb318c6
Pedro <1161028@isep.ipp.pt>
2018-01-09 11:34:49 +0000

    [Implementation] Removed code duplication by calling Main.setLook() :: SelectProjectUI

--- src/main/java/lapr/project/ui/SelectProjectUI.java
+++ src/main/java/lapr/project/ui/SelectProjectUI.java
@@ -40,7 +40,7 @@ public class SelectProjectUI extends javax.swing.JFrame {
     /**
      * Creates new form SelectProjectUI
      */
-    SelectProjectUI() {
+    private SelectProjectUI() {
         selectProjectController = new SelectProjectController(Main.dbCom);
         initComponents();
     }
@@ -223,116 +223,12 @@ public class SelectProjectUI extends javax.swing.JFrame {
     }//GEN-LAST:event_jButtonCompFormActionPerformed
 
     private void jButtonBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBackActionPerformed
-        WelcomeUI.main(null);
+        WelcomeUI.display();
         dispose();
     }//GEN-LAST:event_jButtonBackActionPerformed
 
     private void jButtonPathFormActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonPathFormActionPerformed
-
-        //<editor-fold desc="Description">
-        //        //ToDo Test only; Replace when 'select existing project' is available
-//
-//        RoadNetwork roadNetworkTest = new RoadNetwork("1", "the road network");
-//
-//        Node nodeTest1 = new Node("n01");
-//        Node nodeTest2 = new Node("n02");
-//        Node nodeTest3 = new Node("n03");
-//        Node nodeTest4 = new Node("n04");
-//
-//        roadNetworkTest.addNode(nodeTest1);
-//        roadNetworkTest.addNode(nodeTest2);
-//        roadNetworkTest.addNode(nodeTest3);
-//        roadNetworkTest.addNode(nodeTest4);
-//
-//        List<Double> tollFaresRoadTest = new ArrayList<>();
-//        tollFaresRoadTest.add(0.15);
-//        tollFaresRoadTest.add(0.25);
-//        tollFaresRoadTest.add(0.35);
-//
-//        Road roadTest1 = new Road("A01", "A01", "gantry toll highway");
-//        Road roadTest2 = new Road("A02", "A02", "toll highway", tollFaresRoadTest);
-//        Road roadTest3 = new Road("E01", "E01", "regular road");
-//
-//        Segment segmentTest1 = new Segment(0, 250, 50, 2.5, 10, 2.5, 120, 50);
-//        Segment segmentTest3 = new Segment(0, 50, 250, 25, -30, 1.75, 90, 50);
-//        Segment segmentTest5 = new Segment(0, 250, 50, 50, 10, 2.5, 90, 0);
-//
-//        List<Segment> segmentsTest1 = new ArrayList<>();
-//        segmentsTest1.add(segmentTest1);
-//
-//        List<Segment> segmentsTest2 = new ArrayList<>();
-//        segmentsTest2.add(segmentTest3);
-//
-//        List<Segment> segmentsTest3 = new ArrayList<>();
-//        segmentsTest3.add(segmentTest5);
-//
-//        List<Double> tollFaresSectionTest = new ArrayList<>();
-//        tollFaresSectionTest.add(0.25);
-//        tollFaresSectionTest.add(0.35);
-//        tollFaresSectionTest.add(0.40);
-//
-//        Section sectionTest1 = new Section(nodeTest1, nodeTest2, Direction.BIDIRECTIONAL, segmentsTest1, roadTest1, tollFaresSectionTest);
-//        Section sectionTest2 = new Section(nodeTest2, nodeTest3, Direction.BIDIRECTIONAL, segmentsTest2, roadTest2, new ArrayList<>());
-//        Section sectionTest3 = new Section(nodeTest3, nodeTest4, Direction.BIDIRECTIONAL, segmentsTest3, roadTest3, new ArrayList<>());
-//
-//        roadNetworkTest.addSection(nodeTest1, nodeTest2, sectionTest1);
-//        roadNetworkTest.addSection(nodeTest2, nodeTest3, sectionTest2);
-//        roadNetworkTest.addSection(nodeTest3, nodeTest4, sectionTest3);
-//
-//        List<VelocityLimit> velocityLimitListTest = new ArrayList<>();
-//        velocityLimitListTest.add(new VelocityLimit("highway", new Measurable(110, Unit.KILOMETERS_PER_HOUR)));
-//        velocityLimitListTest.add(new VelocityLimit("road", new Measurable(80, Unit.KILOMETERS_PER_HOUR)));
-//
-//        List<Gears> gearsTest = new ArrayList<>();
-//        gearsTest.add(new Gears(1, 4.5f));
-//        gearsTest.add(new Gears(2, 3.5f));
-//        gearsTest.add(new Gears(3, 2.7f));
-//        gearsTest.add(new Gears(4, 1.6f));
-//        gearsTest.add(new Gears(5, 1.2f));
-//        gearsTest.add(new Gears(6, 0.9f));
-//
-//        List<Regime> regimes25Test = new ArrayList<>();
-//        regimes25Test.add(new Regime(115, 125, 900, 1499, 500));
-//        regimes25Test.add(new Regime(125, 120, 1500, 2499, 450));
-//        regimes25Test.add(new Regime(120, 105, 2500, 3499, 520));
-//        regimes25Test.add(new Regime(105, 90, 3500, 4499, 550));
-//        regimes25Test.add(new Regime(90, 80, 4500, 5500, 650));
-//
-//        List<Regime> regimes50Test = new ArrayList<>();
-//        regimes50Test.add(new Regime(185, 195, 900, 1499, 380));
-//        regimes50Test.add(new Regime(195, 190, 1500, 2499, 350));
-//        regimes50Test.add(new Regime(190, 180, 2500, 3499, 360));
-//        regimes50Test.add(new Regime(180, 150, 3500, 4499, 400));
-//        regimes50Test.add(new Regime(150, 135, 4500, 5500, 520));
-//
-//        List<Regime> regimes100Test = new ArrayList<>();
-//        regimes100Test.add(new Regime(305, 325, 900, 1499, 380));
-//        regimes100Test.add(new Regime(325, 315, 1500, 2499, 350));
-//        regimes100Test.add(new Regime(315, 290, 2500, 3499, 360));
-//        regimes100Test.add(new Regime(290, 220, 3500, 4499, 400));
-//        regimes100Test.add(new Regime(220, 205, 4500, 5500, 520));
-//
-//        List<Throttle> throttlesTest = new ArrayList<>();
-//        throttlesTest.add(new Throttle(25, regimes25Test));
-//        throttlesTest.add(new Throttle(50, regimes50Test));
-//        throttlesTest.add(new Throttle(100, regimes100Test));
-//
-//        Vehicle vehicle1 = new Vehicle("Toyota", "Vehicle 1", VehicleType.Car, 1, Vehicle.MotorType.COMBUSTION, Fuel.Diesel, new Measurable(2000, Unit.KILOGRAM),
-//                new Measurable(0, Unit.KILOGRAM), 0.320f, new Measurable(1.9, Unit.METER_SQUARED), 0.01f, new Measurable(0.6, Unit.METER),
-//                velocityLimitListTest, new Energy(900, 5500, 4f, gearsTest, throttlesTest));
-//
-//        Vehicle vehicle2 = new Vehicle("BMW", "Vehicle 2", VehicleType.Car, 1, Vehicle.MotorType.NONCOMBUSTION, Fuel.Electric, new Measurable(2000, Unit.KILOGRAM),
-//                new Measurable(0, Unit.KILOGRAM), 0.320f, new Measurable(1.9, Unit.METER_SQUARED), 0.01f, new Measurable(0.6, Unit.METER),
-//                velocityLimitListTest, new Energy(900, 5500, 4f, gearsTest, throttlesTest));
-//
-//        List<Vehicle> vehiclesTest = new ArrayList<>();
-//        vehiclesTest.add(vehicle1);
-//        vehiclesTest.add(vehicle2);
-//
-//        Project pjt = new Project("New Project", "Add a description", roadNetworkTest, vehiclesTest); // changed to pjt because of existing project (will be deleted further down the road)
-        //</editor-fold>
-
-        new BestPathUI();
+        BestPathUI.display();
         setVisible(false);
     }//GEN-LAST:event_jButtonPathFormActionPerformed
 
@@ -355,33 +251,10 @@ public class SelectProjectUI extends javax.swing.JFrame {
     }//GEN-LAST:event_jButtonOKActionPerformed
 
     /**
-     * @param args the command line arguments
+     * Triggers UI display
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SelectProjectUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SelectProjectUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SelectProjectUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SelectProjectUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        
-        /* Create and display the form */
+    public static void display() {
+        Main.setLook();
        java.awt.EventQueue.invokeLater(() -> new SelectProjectUI().setVisible(true));
     }
 
