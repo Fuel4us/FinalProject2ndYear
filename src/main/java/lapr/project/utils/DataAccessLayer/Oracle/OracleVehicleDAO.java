24bb832d34dcfc0c50bfd928609a5292da9a1f56
Ana Rita <1161564@isep.ipp.pt>
2018-01-10 19:15:14 +0000

    [LP31718G38-194][Implementation] Corrected procedure name in method storeVehicle :: OracleVehicleDAO

--- src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleVehicleDAO.java
+++ src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleVehicleDAO.java
@@ -368,7 +368,7 @@ public class OracleVehicleDAO extends OracleDAO implements VehicleDAO {
      */
     private void storeVehicle(Vehicle vehicle, String projectName) throws SQLException {
 
-        try (CallableStatement storeVehicleInfoProcedure = oracleConnection.prepareCall("CALL storeVehicleInfoProcedure(?,?,?,?,?,?,?,?,?,?,?,?,?)")) {
+        try (CallableStatement storeVehicleInfoProcedure = oracleConnection.prepareCall("CALL storeVehicleProcedure(?,?,?,?,?,?,?,?,?,?,?,?,?)")) {
 
             storeVehicleInfoProcedure.setString("name", vehicle.getName());
             storeVehicleInfoProcedure.setString("projectName", projectName);
