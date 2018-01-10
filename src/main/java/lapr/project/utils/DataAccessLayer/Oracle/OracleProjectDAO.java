eb9f3f62075d8a95d77051d303e2426b370767e3
Ana Rita <1161564@isep.ipp.pt>
2018-01-10 12:34:11 +0000

    [LP31718G38-210][Implementation] Implemented methods that allow changing the project data, add roads and add vehicles :: OracleProjectDAO

--- src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleProjectDAO.java
+++ src/main/java/lapr/project/utils/DataAccessLayer/Oracle/OracleProjectDAO.java
@@ -92,9 +92,94 @@ public class OracleProjectDAO extends OracleDAO implements ProjectDAO {
             }
 
             storeProjectProcedure.executeUpdate();
+            return true;
         }
 
-        return true;
+    }
+
+    /**
+     * Allows to change {@link Project} column "name"
+     * Has to be called before calling setName(newName) of {@link Project}
+     * @param project {@link Project}
+     * @param projectNewName String identifier of {@link Project}, new name
+     * @return true if success
+     * @throws SQLException
+     */
+    @Override
+    public boolean changeProjectName(Project project, String projectNewName) throws SQLException {
+        verifyConnection();
+
+        try (CallableStatement changeProjectNameProcedure = super.oracleConnection.prepareCall("CALL CHANGEPROJECTNAMEPROCEDURE(?,?)")) {
+
+            String projectOldName = project.getName();
+            changeProjectNameProcedure.setString("oldName", projectOldName);
+            changeProjectNameProcedure.setString("newName", projectNewName);
+
+            changeProjectNameProcedure.executeUpdate();
+            return true;
+        }
+
+    }
+
+    /**
+     * Allows to change {@link Project} column "description"
+     * Has to be called before calling setDescription(newName) of {@link Project}
+     * If name also has to change, has to be called before changeProjectName method
+     * @param project {@link Project}
+     * @param newDescription String identifier of {@link Project}
+     * @return true if success
+     * @throws SQLException
+     */
+    @Override
+    public boolean changeDescription(Project project, String newDescription) throws SQLException {
+        verifyConnection();
+
+        try (CallableStatement changeDescriptionProcedure = super.oracleConnection.prepareCall("CALL changeDescriptionProcedure(?,?)")) {
+
+            String originalName = project.getName();
+            changeDescriptionProcedure.setString("originalName", originalName);
+            changeDescriptionProcedure.setString("newDescription", newDescription);
+
+            changeDescriptionProcedure.executeUpdate();
+            return true;
+        }
+
+    }
+
+    /**
+     * Allows {@link RoadNetwork} growth
+     * @param project {@link Project}
+     * @return true if success
+     * @throws SQLException
+     */
+    @Override
+    public void addRoads(Project project) throws SQLException {
+        verifyConnection();
+
+            RoadNetwork roadNetwork = project.getRoadNetwork();
+            OracleRoadNetworkDAO oracleRoadNetworkDAO = new OracleRoadNetworkDAO();
+            oracleRoadNetworkDAO.connectTo(this.oracleConnection);
+            oracleRoadNetworkDAO.storeRoadNetworkGraph(roadNetwork, roadNetwork.getId());
+    }
+
+    /**
+     * Allows to add more instances of {@link Vehicle}
+     * @param project {@link Project}
+     * @return true if success
+     * @throws SQLException
+     */
+    @Override
+    public void addVehicles(Project project) throws SQLException {
+        verifyConnection();
+
+            String projectName = project.getName();
+
+            List<Vehicle> vehicles = project.getVehicles();
+            OracleVehicleDAO oracleVehicleDAO = new OracleVehicleDAO();
+            oracleVehicleDAO.connectTo(this.oracleConnection);
+            for (Vehicle vehicle : vehicles) {
+                oracleVehicleDAO.storeVehicleInfo(vehicle, projectName);
+            }
     }
 
 
