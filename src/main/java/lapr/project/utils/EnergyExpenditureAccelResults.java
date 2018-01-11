1c16e6d7cc4ea70893cf56867a020ffdefa089db
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-11 08:52:34 +0000

    [Implementation] Added the constructor for the attributes energyExpenditure, finalVelocity and timeSpent in the class EnergyExpenditureAccelResults

--- src/main/java/lapr/project/utils/EnergyExpenditureAccelResults.java
+++ src/main/java/lapr/project/utils/EnergyExpenditureAccelResults.java
@@ -47,6 +47,20 @@ public class EnergyExpenditureAccelResults {
     }
 
     /**
+     * Constructor with the parameters energyExpenditure, finalVelocity and timeSpent
+     * @param energyExpenditure the energy expenditure
+     * @param finalVelocity the final velocity of the vehicle
+     * @param timeSpent the time spent
+     */
+    public EnergyExpenditureAccelResults(Measurable energyExpenditure, Measurable finalVelocity, Measurable timeSpent) {
+        this.energyExpenditure = energyExpenditure;
+        this.finalVelocity = finalVelocity;
+        this.timeSpent = timeSpent;
+        gearForEachSegment = new Gears[]{};
+        tollCosts = new Measurable(0, Unit.EUROS);
+    }
+
+    /**
      * Creates an instance of {@link EnergyExpenditureAccelResults} containing information about travelling time and toll costs.
      * @param timeSpent the time spent in the section
      * @param tollCosts the toll costs for the section
