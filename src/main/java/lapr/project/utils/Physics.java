242bf9503f3fd59c2b67801e0644f4c9b5e871c7
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-12 17:48:58 +0000

    [Implementation] Removed the unused method calculateQuadraticFormula in the class Physics

--- src/main/java/lapr/project/utils/Physics.java
+++ src/main/java/lapr/project/utils/Physics.java
@@ -10,24 +10,6 @@ public class Physics {
     public static final double KILOMETERS_PER_HOUR_METERS_PER_SECOND_CONVERSION_RATIO = 3.6;
     public static final double KILOMETERS_METERS_CONVERSION_RATIO = 0.001;
 
-    /**
-     * Calculates the quadratic formula with the values a, b and c and returns both values stored in an array
-     * @param a the first value
-     * @param b the second value
-     * @param c the third value
-     * @return an array storing the values given by the operation of the quadratic formula
-     */
-    public static double[] calculateQuadraticFormula(double a, double b, double c) {
-        double discriminant = Math.pow(b, 2) - 4 * a * c;
-
-        if (discriminant < 0) {
-            return null;
-        }
-        
-        double value1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        double value2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-
-        return new double[]{value1, value2};
-    }
+    
 
 }
