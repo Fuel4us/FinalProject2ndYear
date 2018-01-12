ad626bb777b31902e2bafee41d0381ab4aa08307
Bruno Pinto <1161063@isep.ipp.pt>
2018-01-12 17:53:54 +0000

    [Implementation] Added the method calculateLinearInterpolation in the class Physics

--- src/main/java/lapr/project/utils/Physics.java
+++ src/main/java/lapr/project/utils/Physics.java
@@ -10,6 +10,19 @@ public class Physics {
     public static final double KILOMETERS_PER_HOUR_METERS_PER_SECOND_CONVERSION_RATIO = 3.6;
     public static final double KILOMETERS_METERS_CONVERSION_RATIO = 0.001;
 
-    
+    /**
+     * Calculates the correspondent value of the valueX given by parameter using the linear interpolation,
+     * according to the intervals X and Y
+     * @param initialX the initial value of the interval X
+     * @param finalX the final value of the interval X
+     * @param initialY the initial value of the interval Y
+     * @param finalY the final value of the interval Y
+     * @param valueX the value correspondent to the interval X
+     * @return the correspondent value in the interval Y
+     */
+    public static double calculateLinearInterpolation(double initialX, double finalX, double initialY, double finalY,
+                                                      double valueX) {
+        return initialY + (finalY - initialY) * (valueX - initialX) / (finalX - initialX);
+    }
 
 }
